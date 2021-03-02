2550dd7120b7d5587084e255526befb14858870e
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-03-02 13:58:42 +0100

    Försättning av interface, och lägger logiken. Continuing of the interface and the logic behind it (in Espresso)

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -1,4 +1,4 @@
-                using System.Collections.Generic;
+using System.Collections.Generic;
 
 public enum CoffeeSorts
 {
@@ -22,56 +22,127 @@ public class Bean
     public CoffeeSorts Sort { get; set; }
 }
 
-    /* 
-     * IBeverage latte = new FluentEspresso()
-                            .AddBeans(new Bean(){ 
-                                AmountInG = 5,
-                                Sort = CoffeSorts.Robusta})
-                            .GrindBeans()
-                            .AddWater(20)
-                            .AddMilk()
-       						.Validate(e => e.Temerature < 80) ** Above 90degree's for espresso
-                        .ToBeverage();
-     */
+/* 
+ * IBeverage latte = new FluentEspresso()
+                        .AddBeans(new Bean(){ 
+                            AmountInG = 5,
+                            Sort = CoffeSorts.Robusta})
+                        .GrindBeans()
+                        .AddWater(20)
+                        .AddMilk()
+                        .Validate(e => e.Temerature < 80) ** Above 90degree's for espresso
+                    .ToBeverage();
+ */
 
+// IBeverage espresso = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 5 && b.Sort = CoffeSorts.Robusta).AddWater().ToBravage();
 public interface IBeverage{
 	List<string> Ingredients { get; }
     CoffeeCup CupType { get; }
     Bean Bean { get; }
     int WaterAmount { get; }
     // int Temperature { get; } ** Maybe we use it depending on the logic for American formula.
-
-   IBeverage AddBeans(Bean bean);
+    IBeverage AddBeans(Bean bean);
     IBeverage GrindBeans();
     IBeverage AddWater(int amount);
     IBeverage AddMilk();
     IBeverage AddMilkFoam();
     IBeverage AddChocolateSyrup();
     IBeverage Validate ();
-
     IBeverage ToBeverage();
 }
 
 class Espresso : IBeverage
 {
-    public List<string> Ingredients = new List<string>() ;
-    public Bean Bean = new Bean();
-    public CoffeeCup esCup => CoffeeCup.Espresso;
+    // This isnt a property, the get or set has to be there to declare a property. In the constructor you would be making the new list.
+    public List<string> Ingredients { get; }
+    // the new bean would be added in the program.cs so you are creating the new bean as perameter.
+    public Bean Bean { get; private set; }
+    public CoffeeCup CupType { get; }
+    public int WaterAmount { get; private set; }
+
+    // contructor to be used in Program.cs
+    public Espresso()
+    {
+        Ingredients = new List<string>();
+    }
+
+    public Espresso(List<string> ingredients)
+    {
+        Ingredients = ingredients;
+    }
+
+    List<string> IBeverage.Ingredients => throw new System.NotImplementedException();
+
+    Bean IBeverage.Bean => throw new System.NotImplementedException();
+
     public IBeverage AddBeans(Bean bean)
     {
         Bean = bean;
         return this;
     }
+
+    public IBeverage AddChocolateSyrup()
+    {
+        Ingredients.Add("Chocolate Syrup");
+        return this;
+    }
+
+    public IBeverage AddMilk()
+    {
+        Ingredients.Add("Milk");
+        return this;
+    }
+
+    public IBeverage AddMilkFoam()
+    {
+        Ingredients.Add("Milk Foam");
+        return this;
+    }
+
     public IBeverage AddWater()
     {
         Ingredients.Add("Water");
         return this;
     }
+
+    public IBeverage AddWater(int amount)
+    {
+        WaterAmount = amount;
+        return this;
+    }
+
+    public IBeverage GrindBeans()
+    {
+        System.Console.WriteLine("Grinding Beans...");
+        return this;
+    }
+
+    public IBeverage ToBeverage()
+    {
+        // Espresso
+        if (Ingredients.Count == 0)
+        {
+            return this;
+        }
+        // Latte
+        if (Ingredients.Count == 1 && Ingredients.Contains("Milk"))
+        {
+
+            return new Latte();
+        }
+        return this;
+    }
+
+    public IBeverage Validate()
+    {
+        throw new System.NotImplementedException();
+    }
 }
 
-class Latte : IBeverage
+class Latte : Espresso
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+    public Latte() : base(new List<string>() { "Milk" })
+    {
 
-    public string CupType => throw new System.NotImplementedException();
-}
+    }
+} 
--- BaristaApi/Program.cs
+++ BaristaApi/Program.cs
@@ -6,11 +6,13 @@ namespace BaristaApi
     {
         static void Main(string[] args)
         {
-            
-            Espresso espresso = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 5 && b.Sort = CoffeSorts.Robusta).ToBravage();
+
+            IBeverage latte = new Espresso().AddBeans(new Bean() { AmmountInG = 9, Sort = CoffeeSorts.Robusta }).AddMilk().ToBeverage();
+            Console.WriteLine(latte.GetType());
+            //IBeverage espresso = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 5 && b.Sort = CoffeSorts.Robusta).ToBravage();
             //espresso is type of Espresso
 
-            Latte latte = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 7 && b.Sort = CoffeSorts.Robusta).AddMilk().ToBravage();
+            //IBeverage latte = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 7 && b.Sort = CoffeSorts.Robusta).AddMilk().ToBravage();
             //latte is type of Latte
             
         }
